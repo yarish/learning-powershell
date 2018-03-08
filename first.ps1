27df327f3b3cc58d099e717c5826ccc66c14eb9e
Janaki Raman <yjanakiraman@deloitte.com>
2018-03-08 13:38:18 +0530

    leanring powershell

---
+++ first.ps1
@@ -0,0 +1,11 @@
+# Set-Location C:\
+# Clear-Host 
+# Get-Command *windowsfeature
+# dism /online enable-feature
+
+# Get-Service Docker
+# Get-Command -noun Service
+# Get-Service -Name AbtSvcHost
+
+# Set-Service "AbtSvcHost" -DisplayName "Haha AbtSvcHost" -WhatIf
+
