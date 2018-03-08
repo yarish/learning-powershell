98657696e141ef6e88919769402a7f5d438cb441
Janaki Raman <yjanakiraman@deloitte.com>
2018-03-08 14:37:09 +0530

    archieve and uncompress files

--- first.ps1
+++ first.ps1
@@ -9,3 +9,24 @@
 
 # Set-Service "AbtSvcHost" -DisplayName "Haha AbtSvcHost" -WhatIf
 
+# Get-Service -Name RetailDemo
+# Get-Command -Noun TimeZone
+# Get-TimeZone
+# Get-TimeZone "India Standard Time"
+# Set-TimeZone -Id "India Standard Time" -WhatIf
+
+Get-Command -Noun Archive
+
+for ($i = 0; $i -lt 20; $i++) {
+    New-Item C:\Temp\MyFakeLogFiles\Logfile_$i.log -Force
+    New-Item C:\Temp\MyFakeLogFiles\Logfile_$i.txt -Force
+}
+
+Get-ChildItem C:\Temp\MyFakeLogFiles\* -Include *.log -Exclude *.txt | Compress-Archive -DestinationPath "C:\Temp\log.zip" 
+
+
+Expand-Archive -Path C:\Temp\log.zip -DestinationPath C:\Temp\LogsOnly 
+
+Get-ComputerInfo -Property windows*
+
+Get-Uptime
